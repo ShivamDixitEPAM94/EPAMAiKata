54c98ee7585d90b8ee57170cd7a02548cb0a413b
ShivamDixitEPAM94 <shivam_dixit@epam.com>
2025-06-03 20:27:45 +0530

    Create src/test/java/stepDefinitions/Hooks.java

---
+++ src/test/java/stepDefinitions/Hooks.java
@@ -0,0 +1,22 @@
+package stepDefinitions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+    WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
