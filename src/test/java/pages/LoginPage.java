118936a6e73dd76f1eb933db50a0ecb70586748b
ShivamDixitEPAM94 <shivam_dixit@epam.com>
2025-06-03 15:16:03 +0530

    Create src/test/java/pages/LoginPage.java

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,34 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage {
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    @FindBy(id = "username")
+    WebElement usernameField;
+
+    @FindBy(id = "password")
+    WebElement passwordField;
+
+    @FindBy(id = "loginButton")
+    WebElement loginButton;
+
+    public void enterUsername(String username) {
+        usernameField.sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        passwordField.sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        loginButton.click();
+    }
+}
