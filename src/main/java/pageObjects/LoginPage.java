9d9255e6cc543b192c888dcb82f3fb71c2ca4088
ShivamDixitEPAM94 <shivam_dixit@epam.com>
2025-06-04 01:04:28 +0530

    Create src/main/java/pageObjects/LoginPage.java

---
+++ src/main/java/pageObjects/LoginPage.java
@@ -0,0 +1,29 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+    WebDriver driver;
+
+    By usernameField = By.id("txtUsername");
+    By passwordField = By.id("txtPassword");
+    By loginButton = By.id("btnLogin");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void enterUsername(String username) {
+        driver.findElement(usernameField).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        driver.findElement(loginButton).click();
+    }
+}
